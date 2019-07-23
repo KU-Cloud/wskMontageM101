01f6ed247d7c57fbf78f932183ee10062c085681
lesomnus <lesomnus@gmail.com>
2019-07-23 20:27:31 +0900

    change incorrect dataset to correct one update server/index.js to log filesize and totla traffic

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const PORT = 5122
 const REPO = 'files'
 
+const fs = require('fs')
+const path = require('path')
+
 const express = require('express')
 const multer = require('multer')
 
@@ -16,11 +19,24 @@ const upload = multer({
 })
 const app = express()
 
+let total = 0;
+
+
+app.use(function (req, res, next) {
+    if (req.method === "GET") {
+        const filename = path.basename(req.url);
+        const stat = fs.statSync(path.join(REPO, filename))
+        const fileSizeInBytes = stat.size;
+        console.log(`${filename} ${fileSizeInBytes} ${total += fileSizeInBytes}`)
+    }
+    next();
+});
 app.use('/files', express.static(REPO))
 
 app.post('/files', upload.any(), (req, res) => {
     for (let file of req.files) {
-        console.log(file.filename)
+        const fileSizeInBytes = file.size;
+        console.log(`${file.filename} ${fileSizeInBytes} ${total += fileSizeInBytes}`)
     }
 
     res.send('uploaded');
